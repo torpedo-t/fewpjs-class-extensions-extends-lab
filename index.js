44c52a6f220c86616c128cd6f728ec9928aca884
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-01-05 16:57:14 -0600

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Your code here
 class Polygon {
     constructor(array) {
-        this.array = array
-        this.count = this.array.length
+        this.sides = array
+        this.count = this.sides.length
     }
 
     get countSides() {
-        return this.array.length
+        return this.sides.length
     }
 
     get perimeter() {
@@ -14,15 +14,16 @@ class Polygon {
         // this.array = [5, 5, 5]
         // console.log(this.array)
         let reducer = (previousValue, currentValue) => previousValue + currentValue;
-        return this.array.reduce(reducer, 0)
+        return this.sides.reduce(reducer, 0)
     }
 }
 
 class Triangle extends Polygon {
     get isValid() {
-        // console.log(this.count)
         if (this.count !== 3) return;
-        // if (this.array[0] + this.array[1] > this.array[2] && this.array[1] + this.array[2] > this.array[0] && this.array[0] + this.array[2] > this.array[1]) return;
+        // console.log(this.sides[0] + this.sides[1] > this.sides[2])
+        // console.log(this.sides[0] + this.sides[1] < this.sides[2])
+        return this.sides[0] + this.sides[1] > this.sides[2] && this.sides[1] + this.sides[2] > this.sides[0] && this.sides[0] + this.sides[2] > this.sides[1];
         // the sum of the lengths of any two sides of a triangle is > than the length of the third side
         // if that is not true, then it is not a triangle
     }
@@ -30,14 +31,16 @@ class Triangle extends Polygon {
 
 class Square extends Polygon {
     get isValid() {
-        // associate a variable with the element in the first index
-        // find the sum of the array
-        // if the square root of the sum of the array is equal to the variable associated with the first element, and the second element then it's a square
+        if (this.count !== 4) return;
+        // console.log(this.sides[0], this.sides[1])
+        return this.sides[0] == this.sides[1] && this.sides[1] == this.sides[2] && this.sides[2] == this.sides[3];
         // the length of all sides must be equal
         // if that is not true, then it is not a square
     }
 
     get area() {
-        // multiply array[0] with array[1]
+        if (!this.sides[0] == this.sides[1] && this.sides[1] == this.sides[2] && this.sides[2] == this.sides[3]) return;
+        return this.sides[0] * this.sides[1]
+        // console.log(this)
     }
 }
