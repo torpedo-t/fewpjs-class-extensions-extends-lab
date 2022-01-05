92d47c9e4127cfb9303d0c5990c17f8ad88d43b9
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-01-05 16:16:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Your code here
 class Polygon {
     constructor(array) {
-        this.array = array;
+        this.array = array
+        this.count = this.array.length
     }
 
     get countSides() {
@@ -19,9 +20,9 @@ class Polygon {
 
 class Triangle extends Polygon {
     get isValid() {
-        // let sides = this.countSides();
-        // console.log(sides)
-        if (this.array[0] + this.array[1] > this.array[2]) && (this.array[1] + this.array[2] > this.array[0]) && (this.array[0] + this.array[2] > this.array[1])
+        // console.log(this.count)
+        if (this.count !== 3) return;
+        // if (this.array[0] + this.array[1] > this.array[2] && this.array[1] + this.array[2] > this.array[0] && this.array[0] + this.array[2] > this.array[1]) return;
         // the sum of the lengths of any two sides of a triangle is > than the length of the third side
         // if that is not true, then it is not a triangle
     }
@@ -31,7 +32,7 @@ class Square extends Polygon {
     get isValid() {
         // associate a variable with the element in the first index
         // find the sum of the array
-        // if the square root of the sum of the array is equal to the variable associated with the first element then it's a square
+        // if the square root of the sum of the array is equal to the variable associated with the first element, and the second element then it's a square
         // the length of all sides must be equal
         // if that is not true, then it is not a square
     }
