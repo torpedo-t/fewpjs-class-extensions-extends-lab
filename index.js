6f597e8666341039cfec585aa730c0ac5858efcc
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-01-05 15:14:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+class Polygon {
+    constructor(array) {
+        this.array = array;
+    }
+
+    get countSides() {
+        return this.array.length
+    }
+
+    get perimeter() {
+        // returns the sum of all elements in array
+        // this.array = [5, 5, 5]
+        // console.log(this.array)
+        let reducer = (previousValue, currentValue) => previousValue + currentValue;
+        return this.array.reduce(reducer, 0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        // the sum of the lengths of any two sides of a triangle is > than the length of the third side
+        // if that is not true, then it is not a triangle
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        // the length of all sides must be equal
+        // if that is not true, then it is not a square
+    }
+
+    get area() {
+        // multiply array[0] with array[1]
+    }
+}
