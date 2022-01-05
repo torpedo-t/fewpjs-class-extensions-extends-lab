9d41405127e5d5265a0f46ed715c9a5894a1edf7
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2022-01-05 15:18:59 -0600

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ class Polygon {
 
 class Triangle extends Polygon {
     get isValid() {
+        if (this.array[0] + this.array[1] > this.array[2]) && (this.array[1] + this.array[2] > this.array[0]) && (this.array[0] + this.array[2] > this.array[1])
         // the sum of the lengths of any two sides of a triangle is > than the length of the third side
         // if that is not true, then it is not a triangle
     }
@@ -26,6 +27,9 @@ class Triangle extends Polygon {
 
 class Square extends Polygon {
     get isValid() {
+        // associate a variable with the element in the first index
+        // find the sum of the array
+        // if the square root of the sum of the array is equal to the variable associated with the first element then it's a square
         // the length of all sides must be equal
         // if that is not true, then it is not a square
     }
